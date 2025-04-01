3f9b488a3b196194cace3917ccb8058de1a49105
IssamiU <issami.umeoka@gmail.com>
2025-04-01 19:47:05 -0300

    Atividade RT2

---
+++ src/mensagens.ts
@@ -0,0 +1,18 @@
+export default class Mensagens {
+
+    public boasVindas = () => {
+        console.log(`\nCalculadora Multifunções.\n`)
+    }
+
+    public listarOpcoes = () => {
+        console.log(`Escolha uma operação disponível:`)
+        console.log(`Somar`)
+        console.log(`Subtrair`)
+        console.log(`Multiplicar`)
+        console.log(`Dividir`)
+        console.log(`Potenciação`)
+        console.log(`Radiciação`)
+        console.log(`Bhaskara (Equações Quadráticas)`)
+        console.log(`\nDigite "Sair" para encerrar o programa.\n`)
+    }
+}
